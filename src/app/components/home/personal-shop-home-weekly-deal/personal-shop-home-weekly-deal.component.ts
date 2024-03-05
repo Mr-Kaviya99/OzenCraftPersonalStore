8d7aa2360255a7651e40fe5c56b4586a63716ff0
Mr-Kaviya99 <kavinduhasintha9@gmail.com>
2024-03-05 13:27:25 +0530

    home weekly deal

--- src/app/components/home/personal-shop-home-weekly-deal/personal-shop-home-weekly-deal.component.ts
+++ src/app/components/home/personal-shop-home-weekly-deal/personal-shop-home-weekly-deal.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CarouselComponent, OwlOptions} from "ngx-owl-carousel-o";
 
 @Component({
   selector: 'app-personal-shop-home-weekly-deal',
   templateUrl: './personal-shop-home-weekly-deal.component.html',
   styleUrls: ['./personal-shop-home-weekly-deal.component.scss']
 })
-export class PersonalShopHomeWeeklyDealComponent {
+export class PersonalShopHomeWeeklyDealComponent implements OnInit {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      475: {
+        items: 2,
+      },
+      575: {
+        items: 3,
+      },
+      800: {
+        items: 4,
+      },
+      1100: {
+        items: 5, // Adjust the number of items for this breakpoint
+      },
+      // nav: true
+    }
+  }
+  carouselData = ['', '', '', '', '', '', '', '', '', '', ''];
 
+  sliderPrev(carousel: CarouselComponent) {
+    carousel.prev();
+  }
+
+  sliderNext(carousel: CarouselComponent) {
+    carousel.next();
+  }
+
+  ngOnInit(): void {
+
+  }
 }
