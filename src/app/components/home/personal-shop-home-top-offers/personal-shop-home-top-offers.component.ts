15d04efb6a86706b7599419602a5107aa63f3945
Mr-Kaviya99 <kavinduhasintha9@gmail.com>
2024-03-06 20:51:54 +0530

    updated

--- src/app/components/home/personal-shop-home-top-offers/personal-shop-home-top-offers.component.ts
+++ src/app/components/home/personal-shop-home-top-offers/personal-shop-home-top-offers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-personal-shop-home-top-offers',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./personal-shop-home-top-offers.component.scss']
 })
 export class PersonalShopHomeTopOffersComponent {
+  imageUrl: string = 'https://wordpressthemes.live/woo/WCM06/WCM060126/WP01/wp-content/uploads/2019/10/slider-img2.png';
 
 }
